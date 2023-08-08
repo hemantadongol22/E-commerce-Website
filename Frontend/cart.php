dcf491e2049f6cf18a4c2ff67c6c81a638c63ee1
Hemanta Dongol <hemantadongol11@gmail.com>
2023-08-08 18:28:13 +0545

    feat: Cart function modular added

--- Frontend/cart.php
+++ Frontend/cart.php
@@ -2,6 +2,13 @@
 session_start();
 include 'includes/header.php';
 
+// Include any necessary files or setup here
+
+// Handle delete action
+if (isset($_GET['delete_product'])) {
+    deleteFromCart(intval($_GET['delete_product']));
+}
+
 // Function to add product to the cart (you can implement other cart operations as well)
 function addToCart($productDetails)
 {
@@ -40,11 +47,6 @@ if (isset($_COOKIE['cartData'])) {
     setcookie('cartData', '', time() - 3600, "/");
 }
 
-if (isset($_GET['delete_product'])) {
-    $deleteProductId = intval($_GET['delete_product']);
-    deleteFromCart($deleteProductId);
-}
-
 // Function to calculate the total cart price
 function calculateTotalPrice()
 {
@@ -74,69 +76,7 @@ function calculateTotalPrice()
             <div class="card col-md-12 ">
                 <div class="card-body">
                     <?php if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0) { ?>
-                        <table class="table table-hover">
-                            <caption></caption>
-                            <thead>
-                                <tr>
-                                    <th class="text-center">S.N.</th>
-                                    <th class="text-center">Product Image</th>
-                                    <th class="text-center">Name</th>
-                                    <th class="text-center">Price</th>
-                                    <th class="text-center">Quantity</th>
-                                    <th class="text-center">Total Price</th>
-                                    <th scope='col'>Options</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $i = 1;
-                                foreach ($_SESSION['cart'] as $productId => $cartItem) {
-                                    $id = $cartItem['id'];
-                                    $ret = mysqli_query($conn, "select * from `product` where id='$id'");
-                                    $row = mysqli_fetch_array($ret);
-                                ?>
-                                    <tr>
-                                        <td>
-                                            <center><?php echo $i++ ?></center>
-                                        </td>
-                                        <td>
-                                            <center><img id="imageDesign" src="../admin/image/<?php echo $row['image']; ?>" alt="#"></center>
-                                        </td>
-                                        <td>
-                                            <center><?php echo $cartItem['name']; ?></center>
-                                        </td>
-                                        <td>
-                                            <center><?php echo $cartItem['price']; ?></center>
-                                        </td>
-                                        <td>
-                                            <center><?php echo $cartItem['quantity']; ?></center>
-                                        </td>
-                                        <td>
-                                            <center><?php echo $cartItem['price'] * $cartItem['quantity']; ?></center>
-                                        </td>
-                                        <td>
-                                            <div class="btn-group">
-                                                <button type="button" class="btn btn-danger">Action</button>
-                                                <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <span class="visually-hidden">Toggle Dropdown</span>
-                                                </button>
-                                                <ul class="dropdown-menu">
-                                                    <li><a class="dropdown-item" href="?delete_product=<?php echo $productId; ?>">Delete</a></li>
-                                                </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                <?php } ?>
-                                <tr>
-                                    <td><strong>Total</strong></td>
-                                    <td colspan="4"></td>
-                                    <td>
-                                        <center><?php echo calculateTotalPrice(); ?></center>
-                                    </td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <?php renderCartTable(); ?>
                         <a href="checkout.php">Proceed to Checkout</a>
                     <?php } else { ?>
                         <div id='fill'>
@@ -151,6 +91,80 @@ function calculateTotalPrice()
     <?php
     include 'includes/footer.php';
     ob_end_flush();
+    ?>
+
+    <?php
+    // Functions
+
+    // Function to render the cart table
+    function renderCartTable()
+    {
+        global $conn;
+        $i = 1;
+    ?>
+        <table class="table table-hover">
+            <caption></caption>
+            <thead>
+                <tr>
+                    <th class="text-center">S.N.</th>
+                    <th class="text-center">Product Image</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Price</th>
+                    <th class="text-center">Quantity</th>
+                    <th class="text-center">Total Price</th>
+                    <th scope='col'>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($_SESSION['cart'] as $productId => $cartItem) {
+                    $id = $cartItem['id'];
+                    $ret = mysqli_query($conn, "select * from `product` where id='$id'");
+                    $row = mysqli_fetch_array($ret);
+                ?>
+                    <tr>
+                        <td>
+                            <center><?php echo $i++ ?></center>
+                        </td>
+                        <td>
+                            <center><img id="imageDesign" src="../admin/image/<?php echo $row['image']; ?>" alt="#"></center>
+                        </td>
+                        <td>
+                            <center><?php echo $cartItem['name']; ?></center>
+                        </td>
+                        <td>
+                            <center><?php echo $cartItem['price']; ?></center>
+                        </td>
+                        <td>
+                            <center><?php echo $cartItem['quantity']; ?></center>
+                        </td>
+                        <td>
+                            <center><?php echo $cartItem['price'] * $cartItem['quantity']; ?></center>
+                        </td>
+                        <td>
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-danger">Action</button>
+                                <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <span class="visually-hidden">Toggle Dropdown</span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="?delete_product=<?php echo $productId; ?>">Delete</a></li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                <?php } ?>
+                <tr>
+                    <td><strong>Total</strong></td>
+                    <td colspan="4"></td>
+                    <td>
+                        <center><?php echo calculateTotalPrice(); ?></center>
+                    </td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php
+    }
     ?>
 </body>
 
